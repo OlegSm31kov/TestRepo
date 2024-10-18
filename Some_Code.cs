a6544ea40856ae7b2fb6810dd85bbae487e2efb7
OlegSm31kov <olegsm31kov@yandex.ru>
2024-10-19 01:38:59 +0300

    Код обновлён v.1.0.0

---
+++ Some_Code.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Prakt_01
+{
+    public class Book
+    {
+        string title;  
+        string author;
+        int year_published;
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+
+        public int YearPublished
+        {
+            get { return year_published; }
+            set { year_published = value; }
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Название: {title};\nАвтор: " +
+                $"{author};\nГод издания: {year_published}.\n");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book dune = new Book();
+
+            dune.Title = "Dune";
+            dune.Author = "Frank Herbert";
+            dune.YearPublished = 1965;
+
+            dune.PrintDetails();
+        }
+    }
+}
